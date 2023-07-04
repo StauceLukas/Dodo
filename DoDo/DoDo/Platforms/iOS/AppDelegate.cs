f65a7730e43eb0efad823d2951d03ca370b663e8
Lukas Staučė <lukasstc223@gmail.com>
2023-07-04 21:39:08 +0300

    Initial project code.

---
+++ DoDo/DoDo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DoDo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
